e1a00135067471c212c8d8447f9ae1b2add77aad
aayushai <aayushhp17@gmail.com>
2024-09-24 14:56:34 +0530

    link-update

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -13,7 +13,10 @@ const Footer = () => {
             <div className="footer-social-icons">
             <img src={assets.facebook_icon} alt="" />
               <img src={assets.twitter_icon} alt="" />
-              <img src={assets.linkedin_icon} alt="" />
+              <a href="http://www.linkedin.com/in/aayushpandey" target="_blank" rel="noopener noreferrer">
+              <img src={assets.linkedin_icon} alt="LinkedIn" />
+              </a>
+             
             </div>
         </div>
         <div className="footer-content-center">
