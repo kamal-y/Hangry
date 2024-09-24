5b4da926c7251bead18ccdf1d738addd461995e3
aayushai <aayushhp17@gmail.com>
2024-09-24 13:21:59 +0530

    frontend-changes-commit

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -29,12 +29,12 @@ const Footer = () => {
             <h2>GET IN TOUCH</h2>
             <ul>
               <li>+1-212-456-7890</li>
-              <li>contact@tomato.com</li>
+              <li>contact@hangry.com</li>
             </ul>
         </div>
       </div>
       <hr/>
-      <p className='footer-copyright'>Copyright 2024 © Tomato.com - All rights reserved.</p>
+      <p className='footer-copyright'>Copyright 2024 © Hangry.com - All rights reserved.</p>
     </div>
   )
 }
